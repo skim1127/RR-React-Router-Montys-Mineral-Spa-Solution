8b0959afb9f6d95e40c7835a58196ee621732e0e
Jin mo Kim <skim112790@yahoo.com>
2022-03-27 10:45:56 -0400

    added bootstrap styling

--- src/components/Packages.js
+++ src/components/Packages.js
@@ -1,18 +1,26 @@
-
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
+import Container from 'react-bootstrap/Container'
 
 export default function Packages(props) {
-    const displayPackages = props.packages.map((eachPackage) => <li>{eachPackage}</li>)
+    const displayPackages = props.packages.map((eachPackage) => 
+    <ListGroup.Item>{eachPackage}</ListGroup.Item>)
 
     return (
-        <div>
-            <div className="packages">
-                <div className="packagesHeader">
-                    <h1>Our Packages</h1>
-                </div>
-                <ul>
+        <Container>
+            <Card>
+                <Card.Body>
+                    <Card.Title>
+                        Our Packages
+                    </Card.Title>
+                    <Card.Text>
+                        Check out some of our packages! Every package is ethically sourced and organic!
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup>
                     {displayPackages}
-                </ul>
-            </div>
-        </div>
+                </ListGroup>
+            </Card>
+        </Container>
     )
 }
